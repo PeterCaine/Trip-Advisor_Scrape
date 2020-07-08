02310aba58e052ad6e8e4be0067e6488ca584867
p.caine <petercaine@gmail.com>
2020-07-08 17:37:29 +0200

    switch between select and indiscriminate scrape

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import os, platform
 from utils import get_links_to_scrape, start_scrape, trip_advisor_scrape
 
-
 def main ():
-    main_url = 'https://www.tripadvisor.com/Airlines'
+    prompt = 'You can scrape en masse (indiscriminantly) or you can scrape select companies. Enter "i" for indiscriminate\
+or "s" for select companies: '
+    main_input = input(prompt)
+    if main_input.lower() == 'i':
+        main_url = 'https://www.tripadvisor.com/Airlines'
+        prompt = "Add number of Trip Advisor pages of companies to scrape: (10 companies per page): "
+        page_input = input(prompt)
+        if page_input == '':
+            page_input = 10
+        else:
+            page_input = int(page_input)
+
         
-    prompt = "Add number of Trip Advisor pages of companies to scrape: (default = 10 @ 5 companies per page - 50 companies): "
-    page_input = input(prompt)
-    if page_input == '':
-        page_input = 10
     else:
-        page_input = int(page_input)
-    
+        prompt = 'enter url of airline you wish to scrape: '
+        main_url = input(prompt)
+        
     prompt = "Add start page (default = 0): "
     start_input = input (prompt)
     if start_input == '':
         start_input = 0
     else:
         start_input = int(start_input)
-        
-    prompt = "Enter number of reviews per company: "
-    num_reviews_input = input (prompt)
-    
-    prompt = "At what point do you wish to start your scrape (page_num - if you have scraped before enter last page, otherwise enter 0): "
-    start = input(prompt)
-        
-    urls = get_links_to_scrape(main_url, start=start_input, num_pages=page_input)
 
+    prompt = "Enter number of review pages you wish to scrape for each airline (5 reviews per page): "
+    num_reviews_input = input (prompt)
+
+    prompt = "if scrape has crashed, change parameter (start) to page number where crash happened to restart from\
+that point otherwise enter 0): "
+    start = input(prompt)
     
+    if main_input.lower() == 'i':
+        urls = get_links_to_scrape(main_url, start=start_input, num_pages=page_input)
+    else:
+        urls = [main_url]
+
+
     for url in urls:
         driver = start_scrape(url, start=int(start_input))
         trip_advisor_scrape(driver, url, start=int(start), num_reviews=int(num_reviews_input))
@@ -36,7 +47,6 @@ def main ():
 if __name__ == "__main__":
     
     if platform.system() == "Linux" or platform.system() == "Linux2" or platform.system() == "Darwin":
-        print (platform.system())
         prompt = "Enter path to geckodriver (default is current working directory): "
         path_input = input(prompt)
         if path_input == '':
@@ -47,4 +57,4 @@ if __name__ == "__main__":
         os.environ["PATH"] += os.pathsep + path
     if not os.path.exists('./airline_pickles'):
         os.mkdir('airline_pickles')
-    main()
+    main() 
